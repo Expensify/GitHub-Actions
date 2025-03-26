d12540744e47c329b9d87dee8cbccfd2931eb940
rory <rory@expensify.com>
2025-03-26 14:48:34 -0700

    Check for unsafe action references

--- .github/scripts/validateActionsAndWorkflows.sh
+++ .github/scripts/validateActionsAndWorkflows.sh
@@ -94,4 +94,35 @@ fi
 # Cleanup after ourselves and delete actionlint
 rm -rf ./actionlint
 
+# Check for unsafe action references
+info 'Checking for mutable action references...'
+find "$GITHUB_DIR" -type f \( -name "*.yml" -o -name "*.yaml" \) -print0 \
+  | xargs -0 grep --no-filename "uses:" \
+  | sed 's/\- uses:/uses:/g' \
+  | tr '"' ' ' \
+  | awk '{print $2}' \
+  | sed 's/\r//g' \
+  | grep -vE '\.github\/' \
+  | sort \
+  | uniq \
+  > /tmp/actionUsages.txt
+
+info 'All action usages...'
+cat /tmp/actionUsages.txt
+
+TRUSTED_ORGS=(
+  '^Expensify/'
+)
+grep -vE "$(IFS='|'; echo "${TRUSTED_ORGS[*]}")" /tmp/actionUsages.txt > /tmp/untrustedActionUsages.txt
+info 'Untrusted action usages...'
+cat /tmp/untrustedActionUsages.txt
+
+GIT_HASH_REGEX='\b[0-9a-f]{40}\b'
+grep -vE "$GIT_HASH_REGEX" /tmp/untrustedActionUsages.txt > /tmp/unsafeActionUsages.txt
+if [ -s /tmp/unsafeActionUsages.txt ]; then
+  error 'Found unsafe mutable action reference to an untrusted action. Use an immutable commit hash reference instead'
+  cat /tmp/unsageActionUsages.txt
+  EXIT_CODE=1
+fi
+
 exit $EXIT_CODE
