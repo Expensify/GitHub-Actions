a2d6c74a4b193511b61ea4f1301ec4c7017e6e01
rory <rory@expensify.com>
2025-03-31 09:45:38 -0700

    Use async.sh for shellCheck as well

--- .github/scripts/shellCheck.sh
+++ .github/scripts/shellCheck.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")
 
 cd "$ROOT_DIR" || exit 1
 
+source ./.github/scripts/utils/async.sh
 source ./.github/scripts/utils/shellUtils.sh
 
 declare -r DIRECTORIES_TO_IGNORE=(
@@ -17,27 +18,22 @@ declare -r DIRECTORIES_TO_IGNORE=(
 # This lists all shell scripts in this repo except those in directories we want to ignore
 read -ra IGNORE_DIRS < <(join_by_string ' -o -path ' "${DIRECTORIES_TO_IGNORE[@]}")
 SHELL_SCRIPTS=$(find . -type d \( -path "${IGNORE_DIRS[@]}" \) -prune -o -name '*.sh' -print)
-info "👀 Linting the following shell scripts using ShellCheck: $SHELL_SCRIPTS"
-info
+info "👀 Linting the following shell scripts using ShellCheck:"
+echo "$SHELL_SCRIPTS"
+echo
 
-ASYNC_PROCESSES=()
 for SHELL_SCRIPT in $SHELL_SCRIPTS; do
   if [[ "$CI" == 'true' ]]; then
     # ShellCheck is installed by default on GitHub Actions ubuntu runners
-    shellcheck -e SC1091 "$SHELL_SCRIPT" &
+    run_async shellcheck -e SC1091 "$SHELL_SCRIPT"
   else
     # Otherwise shellcheck is used via npx
-    npx shellcheck -e SC1091 "$SHELL_SCRIPT" &
+    run_async npx shellcheck -e SC1091 "$SHELL_SCRIPT"
   fi
-  ASYNC_PROCESSES+=($!)
 done
 
-EXIT_CODE=0
-for PID in "${ASYNC_PROCESSES[@]}"; do
-  if ! wait "$PID"; then
-    EXIT_CODE=1
-  fi
-done
+await_async_commands
+EXIT_CODE=$?
 
 cd "$CURRENT_DIR" || exit 1
 
